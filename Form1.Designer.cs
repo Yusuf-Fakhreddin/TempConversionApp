22ab016a424e9fd0750e8a27cffe989f9c64cdc6
Yusuf Fakhreddin <48655310+Yusuf-Fakhreddin@users.noreply.github.com>
2022-01-28 11:37:25 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace TempConversionApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_fahrenheit = new System.Windows.Forms.TextBox();
+            this.txt_celsius = new System.Windows.Forms.TextBox();
+            this.txt_kelvin = new System.Windows.Forms.TextBox();
+            this.btn_fahrenheit = new System.Windows.Forms.Button();
+            this.btn_celsius = new System.Windows.Forms.Button();
+            this.btn_kelvin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fahrenheit";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Location = new System.Drawing.Point(35, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Celsius";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label3.Location = new System.Drawing.Point(42, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kelvin";
+            // 
+            // txt_fahrenheit
+            // 
+            this.txt_fahrenheit.Location = new System.Drawing.Point(105, 40);
+            this.txt_fahrenheit.Name = "txt_fahrenheit";
+            this.txt_fahrenheit.Size = new System.Drawing.Size(100, 20);
+            this.txt_fahrenheit.TabIndex = 3;
+            this.txt_fahrenheit.TextChanged += new System.EventHandler(this.onFahrenheitTextChange);
+            // 
+            // txt_celsius
+            // 
+            this.txt_celsius.Location = new System.Drawing.Point(105, 69);
+            this.txt_celsius.Name = "txt_celsius";
+            this.txt_celsius.Size = new System.Drawing.Size(100, 20);
+            this.txt_celsius.TabIndex = 4;
+            this.txt_celsius.TextChanged += new System.EventHandler(this.onCelsiusTextChange);
+            // 
+            // txt_kelvin
+            // 
+            this.txt_kelvin.Location = new System.Drawing.Point(105, 98);
+            this.txt_kelvin.Name = "txt_kelvin";
+            this.txt_kelvin.Size = new System.Drawing.Size(100, 20);
+            this.txt_kelvin.TabIndex = 5;
+            this.txt_kelvin.TextChanged += new System.EventHandler(this.onKelvinTextChange);
+            // 
+            // btn_fahrenheit
+            // 
+            this.btn_fahrenheit.Location = new System.Drawing.Point(230, 37);
+            this.btn_fahrenheit.Name = "btn_fahrenheit";
+            this.btn_fahrenheit.Size = new System.Drawing.Size(75, 23);
+            this.btn_fahrenheit.TabIndex = 6;
+            this.btn_fahrenheit.Text = "Convert";
+            this.btn_fahrenheit.UseVisualStyleBackColor = true;
+            this.btn_fahrenheit.Click += new System.EventHandler(this.btn_fahrenheit_Click);
+            // 
+            // btn_celsius
+            // 
+            this.btn_celsius.Location = new System.Drawing.Point(230, 65);
+            this.btn_celsius.Name = "btn_celsius";
+            this.btn_celsius.Size = new System.Drawing.Size(75, 23);
+            this.btn_celsius.TabIndex = 7;
+            this.btn_celsius.Text = "Convert";
+            this.btn_celsius.UseVisualStyleBackColor = true;
+            this.btn_celsius.Click += new System.EventHandler(this.btn_celsius_Click);
+            // 
+            // btn_kelvin
+            // 
+            this.btn_kelvin.Location = new System.Drawing.Point(230, 95);
+            this.btn_kelvin.Name = "btn_kelvin";
+            this.btn_kelvin.Size = new System.Drawing.Size(75, 23);
+            this.btn_kelvin.TabIndex = 8;
+            this.btn_kelvin.Text = "Convert";
+            this.btn_kelvin.UseVisualStyleBackColor = true;
+            this.btn_kelvin.Click += new System.EventHandler(this.btn_kelvin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 173);
+            this.Controls.Add(this.btn_kelvin);
+            this.Controls.Add(this.btn_celsius);
+            this.Controls.Add(this.btn_fahrenheit);
+            this.Controls.Add(this.txt_kelvin);
+            this.Controls.Add(this.txt_celsius);
+            this.Controls.Add(this.txt_fahrenheit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_fahrenheit;
+        private System.Windows.Forms.TextBox txt_celsius;
+        private System.Windows.Forms.TextBox txt_kelvin;
+        private System.Windows.Forms.Button btn_fahrenheit;
+        private System.Windows.Forms.Button btn_celsius;
+        private System.Windows.Forms.Button btn_kelvin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempConversionApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // From Fahrenheit
+        private void btn_fahrenheit_Click(object sender, EventArgs e)
+        {
+            convertFromFahrenheit();
+        }
+
+        private void convertFromFahrenheit()
+        {
+            float f = 0;
+
+            if (txt_fahrenheit.TextLength > 0)
+            {
+                try
+                {
+                    f = float.Parse(txt_fahrenheit.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Try Numbers!!");
+                }
+
+
+                float celsiusResult = 5f / 9f * (f - 32);
+                float kelvinResult = celsiusResult - 273;
+
+                txt_celsius.Text = celsiusResult.ToString();
+                txt_kelvin.Text = kelvinResult.ToString();
+            }
+            else
+            {
+
+                txt_celsius.Text = "";
+                txt_kelvin.Text = "";
+            }
+        }
+
+        // From Celsius
+        private void btn_celsius_Click(object sender, EventArgs e)
+        {
+            convertFromCelsius();
+        }
+
+        private void convertFromCelsius()
+        {
+            float c = 0;
+            if (txt_celsius.TextLength > 0)
+            {
+                try
+                {
+                    c = float.Parse(txt_celsius.Text);
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Try Numbers!!");
+                }
+
+
+                float fahrenheitResult = (9f / 5f) * c + 32;
+                float kelvinResult = c + 273;
+
+                txt_fahrenheit.Text = fahrenheitResult.ToString();
+                txt_kelvin.Text = kelvinResult.ToString();
+            }
+            else
+            {
+                txt_fahrenheit.Text = "";
+                txt_kelvin.Text = "";
+
+            }
+        }
+
+        private void btn_kelvin_Click(object sender, EventArgs e)
+        {
+            convertFromKelvin();
+        }
+        private void convertFromKelvin()
+        {
+            float k = 0;
+            if (txt_kelvin.TextLength > 0)
+            {
+                try
+                {
+                    k = float.Parse(txt_kelvin.Text);
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Try Numbers!!");
+                }
+
+
+                float fahrenheitResult = (9f / 5f) * (k - 273) + 32;
+                float celsiusResult = k - 273;
+
+                txt_fahrenheit.Text = fahrenheitResult.ToString();
+                txt_celsius.Text = celsiusResult.ToString();
+            }
+            else
+            {
+                txt_fahrenheit.Text = "";
+                txt_celsius.Text = "";
+            }
+        }
+
+        private void onFahrenheitTextChange(object sender, EventArgs e)
+        {
+            if (!txt_fahrenheit.Focused ) return;
+            convertFromFahrenheit();
+        }
+
+        private void onCelsiusTextChange(object sender, EventArgs e)
+        {
+            if (!txt_celsius.Focused) return;
+
+            convertFromCelsius();
+        }
+
+        private void onKelvinTextChange(object sender, EventArgs e)
+        {
+            if (!txt_kelvin.Focused) return;
+
+            convertFromKelvin();
+        }
+    }
+}
